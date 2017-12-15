50115bdcedad6469012bef322416f95c46cf01eb
Ubuntu <easonlai@QOOOSSDEV01.q1cw0svkxzdefeskwqbhslavad.ix.internal.cloudapp.net>
2017-12-15 15:18:14 +0000

    update to build 10

--- src/main/java/ms/easonlai/Calculator/CalculatorService.java
+++ src/main/java/ms/easonlai/Calculator/CalculatorService.java
@@ -13,7 +13,7 @@ public class CalculatorService {
     @Path("ping")
     @Produces(MediaType.TEXT_PLAIN)
     public String ping() {
-        return "Welcome to Java Web App!!! This is updated via Jenkins with Pipeline build number 9!\n" + new Date().toString();
+        return "Welcome to Java Web App!!! This is updated via Jenkins with Pipeline build number 10!\n" + new Date().toString();
     }
 
     @GET
